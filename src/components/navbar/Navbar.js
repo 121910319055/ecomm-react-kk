a75afa993d4c568e9cf7876128efb6142320ca6e
krishnakarthik <krishnakarthik@lyrostech.com>
2024-03-14 17:54:12 +0530

    2 nd change done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 import App, { UserContext } from "../../App";
 import { AppContext } from "../../context/appContext";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoLogOutSharp } from "react-icons/io5";
 export default function Navbar() {
   const { flag, setFlag, user, setUser } = useContext(UserContext);
   const { cartItems,orders } = useContext(AppContext);
@@ -12,14 +14,15 @@ export default function Navbar() {
   const total = values.length;
   return (
     <div className="navbar">
-      <div className="title">zomato</div>
+      <div className="title">Irish Cafe</div>
       <div><h3>Hi {user.name}!</h3></div>
       <div className="links">
         <Link to={`${PATH}/`}> Products </Link>
         <Link to={`${PATH}/order`}> Orders({orders.length}) </Link>
-        <Link to={`${PATH}/cart`}> Cart({total}) </Link>
+        {total > 0 && ( <Link to={`${PATH}/cart`}><FaShoppingCart /> Cart({total}) </Link>
+      )}
         <Link to={`${PATH}/`} onClick={() => setFlag((prev) => 0)}>
-          Logout
+        <IoLogOutSharp />Logout
         </Link>
       </div>
     </div>
